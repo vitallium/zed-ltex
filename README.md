76764aef3ba840c7339addf297721ecd02874276
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2024-08-06 21:16:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LTeX Language Server
+
+[LTEX Language Server](https://github.com/valentjn/ltex-ls) support for Zed editor
---
+++ src/lib.rs
@@ -0,0 +1,157 @@
+use std::fs;
+
+use zed_extension_api::{
+    self as zed, settings::LspSettings, Command, LanguageServerId, Result, Worktree,
+};
+
+struct LtexBinary {
+    path: String,
+    args: Option<Vec<String>>,
+}
+
+struct LtexExtension {
+    cached_binary_path: Option<String>,
+}
+
+impl LtexExtension {
+    pub const LANGUAGE_SERVER_ID: &'static str = "ltex";
+
+    fn language_server_binary(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<LtexBinary> {
+        if let Some(path) = worktree.which("ltex-ls") {
+            return Ok(LtexBinary {
+                path,
+                args: Some(vec![]),
+            });
+        }
+
+        if let Some(path) = &self.cached_binary_path {
+            if fs::metadata(&path).map_or(false, |stat| stat.is_file()) {
+                return Ok(LtexBinary {
+                    path: path.clone(),
+                    args: Some(vec![]),
+                });
+            }
+        }
+
+        zed::set_language_server_installation_status(
+            &language_server_id,
+            &zed::LanguageServerInstallationStatus::CheckingForUpdate,
+        );
+        let release = zed::latest_github_release(
+            "valentjn/ltex-ls",
+            zed::GithubReleaseOptions {
+                require_assets: true,
+                pre_release: false,
+            },
+        )?;
+
+        let (platform, _arch) = zed::current_platform();
+        let version = release.version;
+
+        let asset_stem = format!(
+            "ltex-ls-{version}-{os}-x64",
+            version = version,
+            os = match platform {
+                zed::Os::Mac => "mac",
+                zed::Os::Linux => "linux",
+                zed::Os::Windows => "windows",
+            }
+        );
+        let asset_name = format!(
+            "{asset_stem}.{suffix}",
+            suffix = match platform {
+                zed::Os::Windows => "zip",
+                _ => "tar.gz",
+            }
+        );
+
+        let asset = release
+            .assets
+            .iter()
+            .find(|asset| asset.name == asset_name)
+            .ok_or_else(|| format!("no asset found matching {:?}", asset_name))?;
+
+        let version_dir = format!("ltex-{}", version);
+        let binary_path = format!("{version_dir}/ltex-ls-{version}/bin/ltex-ls");
+
+        if !fs::metadata(&binary_path).map_or(false, |stat| stat.is_file()) {
+            zed::set_language_server_installation_status(
+                &language_server_id,
+                &zed::LanguageServerInstallationStatus::Downloading,
+            );
+            let file_kind = match platform {
+                zed::Os::Windows => zed::DownloadedFileType::Zip,
+                _ => zed::DownloadedFileType::GzipTar,
+            };
+            zed::download_file(&asset.download_url, &version_dir, file_kind)
+                .map_err(|e| format!("failed to download file: {e}"))?;
+
+            let entries =
+                fs::read_dir(".").map_err(|e| format!("failed to list working directory {e}"))?;
+            for entry in entries {
+                let entry = entry.map_err(|e| format!("failed to load directory entry {e}"))?;
+                if entry.file_name().to_str() != Some(&version_dir) {
+                    fs::remove_dir_all(&entry.path()).ok();
+                }
+            }
+        }
+
+        self.cached_binary_path = Some(binary_path.clone());
+        Ok(LtexBinary {
+            path: binary_path,
+            args: Some(vec![]),
+        })
+    }
+}
+
+impl zed::Extension for LtexExtension {
+    fn new() -> Self {
+        Self {
+            cached_binary_path: None,
+        }
+    }
+
+    fn language_server_command(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &Worktree,
+    ) -> Result<Command> {
+        let ltex_binary = self.language_server_binary(language_server_id, worktree)?;
+
+        Ok(zed::Command {
+            command: ltex_binary.path,
+            args: ltex_binary.args.unwrap(),
+            env: Default::default(),
+        })
+    }
+
+    fn language_server_initialization_options(
+        &mut self,
+        server_id: &LanguageServerId,
+        worktree: &zed_extension_api::Worktree,
+    ) -> Result<Option<zed_extension_api::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.initialization_options.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
+    }
+
+    fn language_server_workspace_configuration(
+        &mut self,
+        server_id: &LanguageServerId,
+        worktree: &zed_extension_api::Worktree,
+    ) -> Result<Option<zed_extension_api::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.settings.clone())
+            .unwrap_or_default();
+        Ok(Some(settings))
+    }
+}
+
+zed::register_extension!(LtexExtension);
